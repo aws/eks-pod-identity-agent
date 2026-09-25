f4e0154200ddd39ead73787ebc65bcaede10d0ef
Gargi Panatula <panatula@amazon.com>
2026-09-25 22:12:38 +0000

    feat(imds): probe + error-ownership on IMDS delegate

--- internal/cloud/imds/service_mocks.go
+++ internal/cloud/imds/service_mocks.go
@@ -67,3 +67,32 @@ func (mr *MockIfaceMockRecorder) GetIamCredentials(ctx, request any) *gomock.Cal
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetIamCredentials", reflect.TypeOf((*MockIface)(nil).GetIamCredentials), ctx, request)
 }
+
+// IsIrrecoverable mocks base method.
+func (m *MockIface) IsIrrecoverable(err error) (string, bool) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "IsIrrecoverable", err)
+	ret0, _ := ret[0].(string)
+	ret1, _ := ret[1].(bool)
+	return ret0, ret1
+}
+
+// IsIrrecoverable indicates an expected call of IsIrrecoverable.
+func (mr *MockIfaceMockRecorder) IsIrrecoverable(err any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "IsIrrecoverable", reflect.TypeOf((*MockIface)(nil).IsIrrecoverable), err)
+}
+
+// String mocks base method.
+func (m *MockIface) String() string {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "String")
+	ret0, _ := ret[0].(string)
+	return ret0
+}
+
+// String indicates an expected call of String.
+func (mr *MockIfaceMockRecorder) String() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "String", reflect.TypeOf((*MockIface)(nil).String))
+}
